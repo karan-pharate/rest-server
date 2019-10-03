33a60dee5190e5b415b1443126248cf863779f29
karan-pharate <karan.pharate@codeastu.com>
2019-10-03 18:26:01 +0530

    created server and added a dataset and get method

---
+++ expressApp/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+let posts = [
+  { id: "1", title: "post 1", description: "some description" },
+  { id: "2", title: "post 2", description: "some description" },
+  { id: "3", title: "post 3", description: "some description" }
+];
+let comments = [
+  {
+    postId: "1",
+    commentData: "some comment",
+    timestamp: 1519211809934
+  },
+  {
+    postId: "2",
+    commentData: "some comment",
+    timestamp: 1519211810362
+  },
+  {
+    postId: "1",
+    commentData: "some comment",
+    timestamp: 15192118132456
+  },
+  {
+    postId: "2",
+    commentData: "some comment",
+    timestamp: 1519211816434
+  },
+  {
+    postId: "3",
+    commentData: "some comment",
+    timestamp: 1519211811670
+  }
+];
+app.get("/api/post", (req, res) => {
+  res.json({ posts });
+});
+app.get("/api/comment/:postId", (req, res) => {
+  let id = req.params.postId;
+  let commentData = comments.filter(comment => {
+    return comment.postId === id;
+  });
+  res.json({ commentData });
+});
+
+app.post("/api/post", (req, res) => {
+   console.log(req.body )
+});
+
+app.listen(port, () => {
+  console.log(`port listening on port ${port}`);
+});
+
